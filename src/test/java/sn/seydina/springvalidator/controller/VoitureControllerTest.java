4134a1ecff956e4633d2da3875b913cef0c3c555
Mouhamed Ndiaye <seydinand18@gmail.com>
2024-11-20 22:41:29 +0000

    ajout tests pour le controller en cas d'erreur

--- src/test/java/sn/seydina/springvalidator/controller/VoitureControllerTest.java
+++ src/test/java/sn/seydina/springvalidator/controller/VoitureControllerTest.java
@@ -41,4 +41,31 @@ class VoitureControllerTest {
 
     }
 
+    @Test
+    void doitRetournerUneErreur() throws Exception {
+        String json = """
+                {
+                    "numeroImmatriculation": "--AA-12345",
+                    "modele": "E",
+                    "annee": 1996,
+                    "kilometerage": -5000,
+                    "prix": -150000000.0
+                }
+                """;
+
+        mockMvc.perform(post("/api/v1/voiture")
+                .content(json)
+                .contentType("application/json"))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.numeroImmatriculation").value("le numero d'immatriculation doit être de la forme XX-12345-XX"))
+                .andExpect(jsonPath("$.marque").value("le nom de la marque doit être fourni"))
+                .andExpect(jsonPath("$.modele").value("le model doit être compris entre 2 et 30 caractères"))
+                .andExpect(jsonPath("$.prix").value("Le prix doit être positif"))
+                .andExpect(jsonPath("$.kilometerage").value("Le kilométrage doit être positif"))
+                .andExpect(jsonPath("$.annee").value("annee invalide"))
+                .andExpect(jsonPath("$.kilometerage").value("Le kilométrage doit être positif"))
+                .andExpect(jsonPath("$.prix").value("Le prix doit être positif"));
+
+    }
+
 }
